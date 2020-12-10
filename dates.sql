19aad203a74e5f627885db87c9a3acc13e147c63
pvongsayarath <64489683+pvongsayarath@users.noreply.github.com>
2020-12-10 09:29:43 -0600

    Add files via upload

---
+++ dates.sql
@@ -0,0 +1,31 @@
+ select date_format('1983-11-1 10:12:35', '%W%M%Y') from people;
+
+insert into people (name,birthdate, birthtime, birthdt) values('microwave','2017-12-12','09:12:55','2017-12-12 09:12:55');
+
+
+SELECT CURTIME();
+ 
+SELECT CURDATE()';
+ 
+SELECT DAYOFWEEK(CURDATE());
+SELECT DAYOFWEEK(NOW());
+SELECT DATE_FORMAT(NOW(), '%w') + 1;
+ 
+SELECT DAYNAME(NOW());
+SELECT DATE_FORMAT(NOW(), '%W');
+ 
+SELECT DATE_FORMAT(CURDATE(), '%m/%d/%Y'');
+ 
+SELECT DATE_FORMAT(NOW(), '%M %D at %h:%i');
+ 
+CREATE TABLE tweets(
+    content VARCHAR(140),
+    username VARCHAR(20),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+ 
+INSERT INTO tweets (content, username) VALUES('this is my first tweet', 'coltscat');
+SELECT * FROM tweets;
+ 
+INSERT INTO tweets (content, username) VALUES('this is my second tweet', 'coltscat');
+SELECT * FROM tweets;
---
+++ insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats(name,age)
+VALUES('Charlie', 17);
+
+INSERT INTO cats(name,age)
+VALUES('Char', 10);
+
+SELECT * FROM cats;
